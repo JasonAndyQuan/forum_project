4b56dd9a7024bb5b28523d5a16931ededdfd97cd
Jason <jasonquan30@gmail.com>
2024-12-04 22:49:23 -0800

    start on db stuff

---
+++ server/config/pool.js
@@ -0,0 +1,9 @@
+import {Pool} from "pg";
+
+export default new Pool({
+    host:process.env.HOST,
+    user:process.env.USER,
+    database:process.env.DATABASE,
+    password:process.env.PASSWORD,
+    port:process.env.DBPORT,
+})
---
+++ server/config/queries.js
@@ -0,0 +1,3 @@
+import db from "./pool.js";
+
+// async function
