036da0d733151e7aa18abe299ab5c545a8e20609
Jason <jasonquan30@gmail.com>
2024-12-27 00:25:14 -0800

    renamed controller files

--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -1,17 +1,3 @@
-/*
-'/posts':  (CRUD)
-    - is the homepage
-    -> '/posts/:id/
-        - an individual text article
-
-// CRUD operations for '/posts'
-// POST /posts: Create a new post
-// GET /posts: Retrieve all posts
-// PUT /posts/:id: Update a post by ID
-// DELETE /posts/:id: Delete a post by ID
-
-
-*/
 
 const {
   createPost,
@@ -22,19 +8,19 @@ const {
   getComments,
   createComment,
   validateContent,
-} = require("../controllers/postsControl.js");
+} = require("../controllers/postsController.js");
 const express = require("express");
 
 const postsRouter = express.Router();
 postsRouter.get("/", getAllPosts); //get all posts
-postsRouter.post("/", validateContent(true), createPost); //make a post
-postsRouter.get("/:id", getPost); //get one post by id
-postsRouter.get("/:id/comments", getComments); //get comments by id
-postsRouter.post("/:id/comments", validateContent(false), createComment); //CREATE COMMENT
+postsRouter.post("/", validateContent(true), createPost); //create a post
+postsRouter.get("/:id", getPost); //get a post by its postid
+postsRouter.get("/:id/comments", getComments); //get comments by postid
+postsRouter.post("/:id/comments", validateContent(false), createComment); //create a comment
 
 
 
-postsRouter.put("/:id", updatePost); //update a post w id
-postsRouter.delete("/:id", deletePost); //delete a post w id
+postsRouter.put("/:id", updatePost); //update a post
+postsRouter.delete("/:id", deletePost); //delete a post
 
 module.exports = postsRouter;
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,8 +1,12 @@
-const { getUserProfile, createUserProfile, validateUser} = require("../controllers/usersControl.js");
+const {
+  getUserProfile,
+  createUserProfile,
+  validateUser,
+} = require("../controllers/usersController.js");
 const express = require("express");
 
 const usersRouters = express.Router();
-usersRouters.get("/:id", getUserProfile);                //get user info via ID
+usersRouters.get("/:id", getUserProfile); //get user info via ID
 usersRouters.post("/", validateUser(), createUserProfile); //create user
 
 module.exports = usersRouters;
